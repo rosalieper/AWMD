d99df345d5b44deb778357b03bca1a3f237c48d8
AhmedToyyib <ahmedtoyyib1@gmail.com>
2019-08-29 13:08:38 +0100

    Toyyib added a java code

---
+++ greetings.java
@@ -0,0 +1,10 @@
+public class Greeting { 
+    public static void main(String[] args) {
+        boolean isRosalieper;
+        if(isRosalieper == true){
+            System.out.print("We are doing well at AWMD");
+        }else{
+            System.out.print("We are not good at AWMD");
+        }
+    }
+}
